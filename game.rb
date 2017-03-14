2d9c63ef71ddee2c1ca8dcdc4b5e268646ad3645
Melissa Khat <melissa.khat@gmail.com>
2017-03-13 21:55:41 -0600

    add methods to check and determine the state of a cell

--- game.rb
+++ game.rb
@@ -17,7 +17,7 @@ class World
 end
 
 class Cell
-  attr_accessor :alive, :x, :y 
+  attr_accessor :alive, :x, :y
 
   #initialize a cell with x and y coordinates
   def initialize(x=0,y=0)
@@ -27,6 +27,23 @@ class Cell
     @alive = false
   end
 
+  #check whether cell is dead or alive
+  def dead?
+    !alive
+  end
+
+  def alive?
+    alive
+  end
+
+  #determine the cell's fate
+  def die
+    @alive = false
+  end
+
+  def live
+    @alive = true
+  end
 
 end
 
